54d3e21a82b897209ecd25a350f558fa1d968dd2
Pratheeksha R <pratheekshar333@gmail.com>
2023-08-06 10:25:36 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,26 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which of the following programming language is used to create programs like applets?",
+            "What is the most popular wired network topology ?",
+            "Which is not an operating system?",
+            "Which of the following kernel is used in android?"
+    };
+
+    public static String choices[][] = {
+            {"Java","C language","COBOL","BASIC"},
+            {"Ring","Mesh","Star","Bus"},
+            {"Linux","DOS","Windows","Oracle"},
+            {"MAC","Linux","Redhat","Windows"}
+    };
+
+    public static String correctAnswers[] = {
+            "Java",
+            "Star",
+            "Oracle",
+            "Linux"
+    };
+
+}
